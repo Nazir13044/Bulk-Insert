80633d2df670026301e0a722fc23a43d350dc569
MD. Nazirul Ahsan <42348515+Nazir13044@users.noreply.github.com>
2019-03-24 11:36:47 +0600

    Update BrandOutletTestControllerController.cs

--- BrandOutletTestControllerController.cs
+++ BrandOutletTestControllerController.cs
@@ -14,6 +14,7 @@ namespace BrandOutlet.Controllers
 {
     public class BrandOutletTestControllerController : Controller
     {
+    //
         private readonly InfoDBEntities _entities = new InfoDBEntities();
         public ActionResult Index()
         {
